dc683ef041fbae7c166826b8ae4442f80db4ef75
Nikola Bogdanovic <bogdanon@mcmaster.ca>
2026-03-29 20:10:14 -0400

    created more routes on react side

--- rnClient/app.js
+++ rnClient/app.js
@@ -9,12 +9,40 @@ import {
     Alert
 } from 'react-native';
 
+
 export default function App() {
+    apiURL = 'http://localhost:3000/api';
+
     const [trips, setTrips] = useState([]);
 
     const [routeName, setRouteName] = useState('');
     const [difficulty, setDifficulty] = useState('');
     const [distance, setDistance] = useState('');
     const [description, setDescription] = useState('');
+
+    const fetchTrips = async () => {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        setTrips(data);
+    };
+
+    const addTrip = async () => {
+        await (apiURL, {
+            method: 'POST', 
+            headers: {'Content-Type': application/json}, 
+            body: JSON.stringify({routeName, difficulty, distance, description})
+        });
+        fetchTrips();
+    };
+
+    const deleteTrips = async () => {
+        await fetch(apiURL, {method:'DELETE'});
+        fetchTrips();
+    };
+
+    const fetchTrip = async () => {
+        const response = await fetch(`${API_URL}/${id}`);
+        return await response.json();
+    }
 
 }
