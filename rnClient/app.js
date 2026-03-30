bde02ada997321d9eba8c0a9d95f39b1279f575f
Nikola Bogdanovic <bogdanon@mcmaster.ca>
2026-03-29 20:35:15 -0400

    replaced files to build folder with expo

--- rnClient/app.js
+++ rnClient/app.js
@@ -1,62 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import {
-    View,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    FlatList,
-    StyleSheet,
-    Alert
-} from 'react-native';
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    apiURL = 'http://localhost:3000/api';
-
-    const [trips, setTrips] = useState([]);
-
-    const [routeName, setRouteName] = useState('');
-    const [difficulty, setDifficulty] = useState('');
-    const [distance, setDistance] = useState('');
-    const [description, setDescription] = useState('');
-
-    const fetchTrips = async () => {
-        const response = await fetch(apiURL);
-        const data = await response.json();
-        setTrips(data);
-    };
-
-    const addTrip = async () => {
-        await (apiURL, {
-            method: 'POST', 
-            headers: {'Content-Type': application/json}, 
-            body: JSON.stringify({routeName, difficulty, distance, description})
-        });
-        fetchTrips();
-    };
-
-    const deleteTrips = async () => {
-        await fetch(apiURL, {method:'DELETE'});
-        fetchTrips();
-    };
-
-    const fetchTrip = async (id) => {
-        const response = await fetch(`${apiURL}/${id}`);
-        return await response.json();
-    };
-
-    const updateTrip = async (id) => {
-        await fetch(`${apiURL}/${id}`, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ routeName, difficulty, distance, description })
-        });
-        fetchTrips();
-    };
-
-    const deleteTrip = async (id) => {
-        await fetch(`${apiURL}/${id}`, {method:'DELETE'});
-        fetchTrips();
-    }
-
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
